50e120497f6630f496a5c40c493fc65a70a8565d
Bennett Lang <84034168+Bennebotix@users.noreply.github.com>
2023-01-16 11:57:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     const mockttp = require('mockttp');
     const https = await mockttp.generateCACertificate();
     const server = mockttp.getLocal({ https });
-    await server.get("/admin/api").withQuery({ cashId: 9375 }).thenReply(200, "<code>YAY!</code>");
+    await server.get("/admin/api").withQuery({ cashId: 9375 }).thenForwardTo("https://www.wikipedia.org");
     await server.start();
     const caFingerprint = mockttp.generateSPKIFingerprint(https.cert);
     if (process.argv[2] === 'chrome') {
